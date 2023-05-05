67c0a5ae5236fc2c1309cb7ffdb45f0737dd62f3
omarmokhtar01 <omarmokh79@gmail.com>
2023-05-05 23:11:21 +0300

    four commit

--- Controller/brandServices.js
+++ Controller/brandServices.js
@@ -4,14 +4,8 @@ const { v4: uuidv4 } = require("uuid");
 const sharp = require("sharp");
 const asyncHandler = require("express-async-handler");
 const { createImgMiddleware } = require("../middlewares/imgMiddleware");
-const cloudinary = require("cloudinary").v2;
-const path = require("path");
 
-cloudinary.config({
-  cloud_name: "dn05vqdfg",
-  api_key: "577477985636914",
-  api_secret: "Q7nONA5KAFhsl_A8kTNwsOPc5U4",
-});
+
 // @desc Control image category size and quality
 // @access Private
 exports.sharpImageBrand = asyncHandler(async (req, res, next) => {
@@ -26,19 +20,6 @@ exports.sharpImageBrand = asyncHandler(async (req, res, next) => {
     req.body.image = filename;
   }
   next();
-});
-
-// @desc add image to cloudinary
-// @access Private
-exports.imageCloudinaryBrand = asyncHandler(async (req, res, next) => {
-  console.log(cloudinary.uploader.upload());
-  // cloudinary.v2.uploader.upload(
-  //   "/home/my_image.jpg",
-  //   { upload_preset: "my_preset" },
-  //   (error, result) => {
-  //     console.log(result, error);
-  //   }
-  // );
 });
 
 // @desc Create image category
